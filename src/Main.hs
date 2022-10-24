abd9210439022924ad85d932b1e256db6dd6fab8
Olaf Boekholt <o.w.boekholt@students.uu.nl>
2022-10-24 15:36:18 +0200

    main.hs updated to asteroids

--- src/Main.hs
+++ src/Main.hs
@@ -7,9 +7,9 @@ import View
 import Graphics.Gloss.Interface.IO.Game
 
 main :: IO ()
-main = playIO (InWindow "Counter" (400, 400) (0, 0)) -- Or FullScreen
+main = playIO (InWindow "Asteroids" (800, 800) (300, 100)) -- Or FullScreen
               black            -- Background color
-              10               -- Frames per second
+              30               -- Frames per second
               initialState     -- Initial state
               view             -- View function
               input            -- Event function

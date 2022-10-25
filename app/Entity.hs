b90b0fd88060bba2f4427708e5e4046539f02c17
Olaf Boekholt <o.w.boekholt@students.uu.nl>
2022-10-25 20:30:25 +0200

    entity vec and pt imports

--- app/Entity.hs
+++ app/Entity.hs
@@ -1,10 +1,13 @@
 module Entity where
 
+import Graphics.Gloss.Data.Point
+import Graphics.Gloss.Data.Vector
+
 data Entity = MkEntity { size      :: Int
                        , position  :: Point
                        , direction :: Vector 
                        , speed     :: Float --pixels p/tick (30/s)
                        }
 
-data Vector = Vec Float Float
-data Point = Pt Float Float
+--data Vector = Vec Float Float
+--data Point = Pt Float Float

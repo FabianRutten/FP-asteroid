c3905f333870c5d2e7dbe8ed8105b69a9e26b7b1
FabianRutten <f.w.rutten@students.uu.nl>
2022-10-26 13:25:32 +0200

    updatePoint -> undefined

--- app/Tick.hs
+++ app/Tick.hs
@@ -21,13 +21,21 @@ updateSaucers s = s {saucers = map updateSaucer $ saucers s}
 
 updatePlayer :: Space -> Space
 updatePlayer s = let update p = p {ship = updateShipPosition (ship p)}
-                     go = undefined
-                in s {player = update $ player s}
+                 in s {player = update $ player s}
 
 updateShipPosition :: Entity -> Entity
-updateShipPosition ship = ship {position = update $ position ship}
-                    where
-                         update p = mulSV (speed ship) (direction ship) `addPoint` p
+updateShipPosition ship = let newPoint = newShipPoint (position ship) ship
+                          in ship {position = updatePoint newPoint}
+                    
+newShipPoint :: Point -> Entity -> Point
+newShipPoint p ship = mulSV (speed ship) (direction ship) `addPoint` p
+
+updatePoint :: Point -> Point --screensize + 100 (dark place) as maximum point values. with negatives as well -> swap other angle and swap sides
+updatePoint new@(x,y) | x > 800 + 100 = undefined
+                      | y > 800 + 100 = undefined
+                      | x < (-100) = undefined
+                      | y < (-100) = undefined
+                      | otherwise =  new
 
 updateAsteroid :: Asteroid -> Asteroid
 updateAsteroid = undefined

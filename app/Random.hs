f4a88b6ac7f76663ed8c9bfb038d10399d9adbab
Olaf Boekholt <o.w.boekholt@students.uu.nl>
2022-11-10 14:36:21 +0100

    random location

--- app/Random.hs
+++ app/Random.hs
@@ -2,7 +2,11 @@ module Random where
 
 import System.Random
 import Graphics.Gloss.Data.Vector
+import Graphics.Gloss.Data.Point
 import System.Random.Stateful (STGen)
+
+randomLocation :: StdGen -> (Point, StdGen)
+randomLocation gen = undefined
 
 randomDirection :: StdGen -> (Vector, StdGen)
 randomDirection gen = ((outputx, ouputy),newSeed)

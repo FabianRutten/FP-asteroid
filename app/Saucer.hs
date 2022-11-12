6ca4d99c96776ddfb70736ba31b14391add42b1e
FabianRutten <f.w.rutten@students.uu.nl>
2022-11-12 19:27:08 +0100

    saucer.hs

---
+++ app/Saucer.hs
@@ -0,0 +1,18 @@
+module Saucer where
+
+import Model
+import Random
+import Graphics.Gloss (Point)
+
+
+pickNewDirection :: Saucer -> Saucer
+pickNewDirection = undefined
+
+saucerFireBullet :: Space -> Space
+saucerFireBullet = undefined
+
+closestsFrontCollision :: Saucer -> [Asteroid] -> Point
+closestsFrontCollision = undefined
+
+
+
